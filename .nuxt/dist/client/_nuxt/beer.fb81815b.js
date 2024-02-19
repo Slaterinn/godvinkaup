163aaa4081a50d1e70bfab1e837b54a90a0c164b
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2024-02-19 09:44:02 +0000

    Updates

--- .nuxt/dist/client/_nuxt/beer.fb81815b.js
+++
@@ -1,1 +0,0 @@
-import{u as e,c as t,o as r}from"./entry.3a129413.js";const n={__name:"beer",setup(s){return e({title:"Bjórar"}),(a,c)=>(r(),t("div",null," This is the beers site "))}};export{n as default};
--- .nuxt/dist/server/_nuxt/index-styles-1.mjs-3809afd9.js
+++
@@ -1,5 +0,0 @@
-const Carousel_vue_vue_type_style_index_0_scoped_f5508169_lang = ".no-scrollbar[data-v-f5508169]::-webkit-scrollbar{display:none}.no-scrollbar[data-v-f5508169]{-ms-overflow-style:none;scrollbar-width:none}";
-export {
-  Carousel_vue_vue_type_style_index_0_scoped_f5508169_lang as default
-};
-//# sourceMappingURL=index-styles-1.mjs-3809afd9.js.map
