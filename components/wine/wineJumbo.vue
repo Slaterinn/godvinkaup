31f5755680aa384cc92e8ee6e54f197c4672c9f3
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2024-02-22 20:07:37 +0000

    smá litabreytingar

--- components/wine/wineJumbo.vue
+++ components/wine/wineJumbo.vue
@@ -1,7 +1,6 @@
 <template>
   <section >
-    <div class="pt-[150px] pb-[50px] px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-    
+    <div class="pt-[150px] pb-[50px] px-4 mx-auto max-w-screen-xl text-center">
         <h1 class="mb-4 text-5xl font-teko tracking-widest leading-none  md:text-6xl lg:text-8xl text-[#FDA47E] dark:text-[#FDA47E] font-bold">GÓÐ VÍNKAUP</h1>
         <p class="mb-8 text-lg font-montserrat text-[#FDA47E] lg:text-2xl sm:px-16 lg:px-48 dark:text-[#FDA47E]">
           Hér er hægt að leita í vínlista ÁTVR og Sante ásamt einkunn þeirra frá Vivino. Einungis er um að ræða 750ml flöskur því allt annað er bara rugl.
