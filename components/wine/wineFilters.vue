968b54c90d95937b86384a95d732f276223f1d05
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2024-02-23 10:52:49 +0000

    filter modal

--- components/wine/wineFilters.vue
+++ components/wine/wineFilters.vue
@@ -92,45 +92,55 @@ const resetFilters = () => {
 </script>
 
 <template>
-<div class="mx-auto max-w-3xl md:w-full bg-[#FFE7D4] rounded-lg shadow m-5 text-[#3E3737]">
-  <div class="m-2 max-w-screen-md">
-    <div class="rounded-xl p-6">
-      <h2 class=" text-xl font-bold">Þrengdu niðurstöðurnar</h2>
-      <p class="mt-1 text-sm text-[#7a7a7a]">Prófaðu síurnar. Með leitinni er hægt að leita að ýmsum eiginleikum, eins og nafni, árgerð og landi.</p>
-      <div class="mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <div class="flex flex-col">
-          <label for="searchAll" class="text-stone-600 text-sm font-medium">Leit</label>
-          <input type="text" v-model="searchInput" id="searchAll" placeholder="prófaðu leitina" class="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
+  <!-- Modal toggle -->
+<div class="mx-auto max-w-3xl md:w-full font-montserrat">
+  <button data-modal-target="wine-filters" data-modal-toggle="wine-filters" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-[#897171] rounded-lg hover:bg-[#A48B8A] focus:ring-4 focus:outline-none focus:ring-black-200  dark:hover:bg-[#A48B8A] dark:focus:ring-black-700 ms-3" type="button">
+    Síur
+  </button>
+</div>
+<div id="wine-filters" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full font-montserrat">
+  <div class="mx-auto max-w-3xl md:w-full bg-[#FFE7D4] rounded-lg shadow m-5 text-[#3E3737]">
+    <div class="m-2 max-w-screen-md">
+      <div class="rounded-xl p-6">
+        <h2 class=" text-xl font-bold">Þrengdu niðurstöðurnar</h2>
+        <p class="mt-1 text-sm text-[#7a7a7a]">Prófaðu síurnar. Með leitinni er hægt að leita að ýmsum eiginleikum, eins og nafni, árgerð og landi.</p>
+        <div class="mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div class="flex flex-col">
+            <label for="searchAll" class="text-stone-600 text-sm font-medium">Leit</label>
+            <input type="text" v-model="searchInput" id="searchAll" placeholder="prófaðu leitina" class="mt-2 block w-full rounded-md text-[#3E3737] placeholder-gray-400 bg-gray-100 border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
+          </div>
+
+
+          <div class="flex flex-col">
+            <label for="wineType" class="text-stone-600 text-sm font-medium">Tegund</label>
+            <select id="wineType" v-model="wineAttributes.category" class="mt-2 block w-full rounded-md text-[#3E3737] border bg-gray-100 border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+              <option>Allar</option>
+              <option>Rauðvín</option>
+              <option>Hvítvín</option>
+              <option>Rósavín</option>
+              <option>Freyðivín</option>
+            </select>
+          </div>
+
+          <div class="flex flex-col">
+            <label for="winePriceMin" class="text-stone-600 text-sm font-medium">Lágmarksverð</label>
+            <input type="number" id="winePriceMin" placeholder="Lágmarksverð" v-model="wineAttributes.min" class="mt-2 block w-full rounded-md text-[#3E3737] placeholder-gray-400 bg-gray-100 border border-gray-200  px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
+          </div>
+
+          <div class="flex flex-col">
+            <label for="winePriceMin" class="text-stone-600 text-sm font-medium">Hámarksverð</label>
+            <input type="number" id="winePriceMax" placeholder="Hámarksverð" v-model="wineAttributes.max" class="mt-2 block w-full rounded-md text-[#3E3737] placeholder-gray-400 bg-gray-100 border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
+          </div>
         </div>
 
-
-        <div class="flex flex-col">
-          <label for="wineType" class="text-stone-600 text-sm font-medium">Tegund</label>
-          <select id="wineType" v-model="wineAttributes.category" class="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
-            <option>Allar</option>
-            <option>Rauðvín</option>
-            <option>Hvítvín</option>
-            <option>Rósavín</option>
-            <option>Freyðivín</option>
-          </select>
+        <div class="mt-6 grid w-full grid-cols-2 justify-end space-x-4 md:flex font-teko text-m">
+          <button @click ="resetFilters" 
+                  class="active:scale-95 rounded-lg text-gray-900 bg-gray-300 px-8 py-2 font-medium  outline-none focus:ring hover:opacity-90"
+                  data-modal-hide="wine-filters">Endurstilla</button>
+          <button @click="onChangeFilter"
+                  class="active:scale-95 rounded-lg px-8 py-2 font-medium text-white bg-[#625A5A] outline-none focus:ring hover:opacity-90"
+                  data-modal-hide="wine-filters">Leita</button>
         </div>
-
-        <div class="flex flex-col">
-          <label for="winePriceMin" class="text-stone-600 text-sm font-medium">Lágmarksverð</label>
-          <input type="number" id="winePriceMin" placeholder="Lágmarksverð" v-model="wineAttributes.min" class="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
-        </div>
-
-        <div class="flex flex-col">
-          <label for="winePriceMin" class="text-stone-600 text-sm font-medium">Hámarksverð</label>
-          <input type="number" id="winePriceMax" placeholder="Hámarksverð" v-model="wineAttributes.max" class="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
-        </div>
-      </div>
-
-      <div class="mt-6 grid w-full grid-cols-2 justify-end space-x-4 md:flex">
-        <button @click ="resetFilters" 
-                class="active:scale-95 rounded-lg bg-gray-200 px-8 py-2 font-medium text-gray-600 outline-none focus:ring hover:opacity-90">Endurstilla</button>
-        <button @click="onChangeFilter"
-                class="active:scale-95 rounded-lg bg-blue-600 px-8 py-2 font-medium text-white outline-none focus:ring hover:opacity-90">Leita</button>
       </div>
     </div>
   </div>
