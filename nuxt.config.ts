6019319b1511192a98ee2512749783b6e4c01b38
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2024-03-19 15:26:52 +0000

    ga4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,12 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui',
             '@nuxtjs/sitemap',
             '@nuxtjs/robots',
-            'nuxt-icon',],
+            'nuxt-icon'],
+  runtimeConfig: {
+    public: {
+      gtagId: 'G-D0Q8GFPQZ6',
+    }
+  },
   nitro: {
     compressPublicAssets: {
       brotli: true
