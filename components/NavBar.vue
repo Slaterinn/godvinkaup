c371f4a56b268e9751c3df71ac205558d0d1b687
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2024-03-19 14:53:33 +0000

    prófa að skrá google

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,36 +1,5 @@
 <template>
-  <!--<div class="fixed top-0 left-0 right-0 z-50 flex justify-between items-center space-x-1 bg-[#020201] text-[#FDA47E] p-4 shadow-md">
-    <div>
-      <NuxtLink class="text-3xl font-teko" to="/">Góð Vínkaup</NuxtLink>
-    </div>
-
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0"> 
-        <li>
-          <NuxtLink class="text-xl font-teko" to="/wine">Vín</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink class="text-xl font-teko" to="/beer">Bjór</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink class="text-xl font-teko" to="/about">Um GK</NuxtLink>
-        </li>
-
-      </ul>
-      
-    </div>
-    
-  </div>-->
-
-
-  
+ 
 
 <nav class="fixed top-0 left-0 right-0 z-50 bg-[#2A2424] text-[#FDA47E] ">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
