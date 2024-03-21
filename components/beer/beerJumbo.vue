33fbb4142bfbb353e07d9782f68eb75fd51492b1
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2024-03-21 09:10:26 +0000

    innsláttarvilla

--- components/beer/beerJumbo.vue
+++ components/beer/beerJumbo.vue
@@ -3,7 +3,7 @@
 <div class="pt-[150px] pb-[50px] px-4 mx-auto max-w-screen-xl text-center">
         <h1 class="mb-4 text-5xl font-teko tracking-widest leading-none  md:text-6xl lg:text-8xl text-[#FDA47E] dark:text-[#FDA47E] font-bold">BJÓR</h1>
         <p class="mb-8 text-lg font-montserrat text-[#FDA47E] lg:text-2xl sm:px-16 lg:px-48 dark:text-[#FDA47E] text-justify">
-          Hér er hægt að sjá alla bjóra sem eru í sölu hjá ÁTVR ásamt viðbættri einkunn þeirra frá Untappd. Hægt er að nota síðuna til þess að þrengja leitina og leitin sjálf leitar í hinum ýmsu eiginleikum. Það er best að prófa!
+          Hér er hægt að sjá alla bjóra sem eru í sölu hjá ÁTVR ásamt viðbættri einkunn þeirra frá Untappd. Hægt er að nota síuna til þess að þrengja leitina og leitin sjálf leitar í hinum ýmsu eiginleikum. Það er best að prófa!
         </p>
     </div>
 </template>
