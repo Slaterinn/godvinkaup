57e5a1cb39147aeb39a3c7c868c68b412e4f8ddb
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2024-03-25 20:35:51 +0000

    laga cononical

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <script setup>
 useHead({
   htmlAttrs: {lang: 'is'},
-  link: [{ rel: 'canonical', href: 'https://www.godvinkaup.is/' },
+  link: [{ rel: 'canonical', href: 'https://godvinkaup.is/' },
          { rel: 'apple-touch-icon', href: '~/assets/images/touch_icon.png' }]
 })
 useSeoMeta({
-  title: 'Góð Vínkaup',
-  ogTitle: 'Góð Vínkaup',
-  description: 'Góð Vínkaup hjálpar þér að finna góð vín og bjór á betra verði.',
+  title: 'Gerðu bara góð vínkaup',
+  ogTitle: 'Gerðu bara góð vínkaup',
+  description: 'Góð Vínkaup hjálpar þér að finna góð vín og góðan bjór á betra verði með tengingum við þekkt forrit fyrir endurgjöf.',
   ogDescription: 'Góð Vínkaup hjálpar þér að finna góð vín og góðan bjór á betra verði með tengingum við þekkt forrit fyrir endurgjöf.',
   ogImage: '~/assets/images/background.jpg',
   ogLocale: 'is-IS',
