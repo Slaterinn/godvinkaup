42fb315060c37fd278a9a697db0887efdd436048
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2024-04-12 22:52:12 +0000

    ný meðmæli

--- components/landing/ProductCarousel.vue
+++ components/landing/ProductCarousel.vue
@@ -16,7 +16,7 @@ const items1 = [{
   }, {
     name: 'Masso Antico Primitivo',
     img: 'https://images.vivino.com/thumbs/q8BUc8n8Qkivs11SxrXZqg_pb_x600.png',
-    description: 'Búið til með appassimento þurrk-aðferðinni en það skilar sér þéttu víni dökkum lit og meira afgerandi bragði og ilm af svörtum skógarávöxtum og reyk. Ertu að fara að grilla í sól og 5 stiga hita? Gríptu þá þessa með og paraðu með grillkjötinu og þú verður ekki fyrir vonbrigðum!',
+    description: 'Búið til með appassimento þurrk-aðferðinni en það skilar sér þéttu víni, dökkum lit og meira afgerandi bragði og ilm af svörtum skógarávöxtum og reyk. Ertu að fara að grilla í sól og 5 stiga hita? Gríptu þá þessa með og paraðu með grillkjötinu og þú verður ekki fyrir vonbrigðum!',
     seller: 'ÁTVR',
     price: '2.590 kr',
     grapes: 'Primitivo',
@@ -69,47 +69,71 @@ const items1 = [{
 
 
   const items2 = [{
-    name: 'Quattronotti Appassimento Negroamaro',
-    img: 'https://images.vivino.com/thumbs/Cu-HZ2XwR8evPLwpoR1kkA_pb_x600.png',
-    description: 'Dökkkirsuberjarautt, þétt og mikið vín frá Puglia á Ítalíu. Vínið 100% úr negroamaro þrúgunni sem gefur því ekki bara dökkan lit heldur bragð af brómberjum og þurrkuðu kryddi eins og blóðbergi.',
+    name: 'Antieri susumaniello',
+    img: 'https://images.vivino.com/thumbs/-cl2YA2cSAO65wUQPKsmuw_pb_x600.png',
+    description: 'Kröftugt og ríkt rautt vín sem ber með sér kraftmikinn ilm og bragð og kryddaðri plómusultu með negul og tóbaki. Þétt ávaxtabragðið fær faðmlag frá fíngerðu tanníni og kanill kyssir það bless. Þetta er eiginlega rosalegt vín og það verður að fá að anda í góðan tíma til að losa um epískt bragðið.',
     seller: 'ÁTVR',
-    price: '3.716 kr',
-    grapes: 'Negroamaro 100%',
-    country: 'Ítalía',
+    price: '4.494 kr',
+    grapes: 'Susumaniello',
+    country: 'Ítalía - Puglia',
+    link_store: 'https://www.vinbudin.is/heim/vorur/stoek-vara.aspx/?productid=29650',
+    link_vivino: 'https://www.vivino.com/US-CA/en/schola-sarmenti-antieri-susumaniello/w/5424453',
+    rating: '4.3',
     wineType: 'Rauðvín',
     wineColor: 'text-[#a82548]'
   }, {
-    name: 'The Chocolate Block',
-    img: 'https://images.vivino.com/thumbs/jQ-ODF_jRpuqCxlRytxYLw_pb_x600.png',
-    description: 'Skemmtilegt vín frá Suður-Afríku með undirliggjandi kryddbragð og rifsberjakeim. Vín með mikið body og eftirbragð sem situr lengi eftir. Hentar vel með rauðu kjöti.',
+    name: 'Reserva XR',
+    img: 'https://images.vivino.com/thumbs/B16FOepaS1aO5WPtys9Hdg_pb_x600.png',
+    description: 'Djúpkirsuberjarautt vín með skærum blæ á brún. Ekki ósvipað góðum Bordeaux-vínum er þetta vín örlítið lokað til að byrja með en opnar sig þegar þú færð að kynnast því. Reyktur og kryddaður ilmur af múskati og kanil kemur fljótt í ljós ásamt lakkrís og bláberjum. Í lokin er ekki fráleitt að finna snefil af balsamik. Vín sem þú verður hreinlega að prófa!',
     seller: 'ÁTVR',
-    price: '3.995 kr',
-    grapes: 'Syrah 73%, Grenache 11%, Cinsault 8%, Cabernet Sauvignon 7%, Viognier 1%',
-    country: 'Suður-Afríka',
+    price: '4.999 kr',
+    grapes: 'Tempranillo, Graciano',
+    country: 'Spánn - Rioja',
+    link_store: 'https://www.vinbudin.is/heim/vorur/stoek-vara.aspx/?productid=29493',
+    link_vivino: 'https://www.vivino.com/US-CA/en/marques-de-riscal-rioja-reserva-xr/w/7388608',
+    rating: '4.3',
     wineType: 'Rauðvín',
     wineColor: 'text-[#a82548]'
   }, {
-    name: '2011 Viña Bosconia Tinto Reserva',
-    img: 'https://images.vivino.com/thumbs/eDyHmY-EQcG28_Ezlpgq5A_pb_x600.png',
-    description: 'Þetta vín hefur yfir meira en áratug þróast yfir í djúpan rauðan lit með örlítilli appelsínugulu. Lyktin ber með sér ágenga ávexti og bragðið er þétt, mjúkt og fínt (þetta er ekki lýsing á rúminu mínu)',
+    name: 'Cornas Hypsos',
+    img: 'https://images.vivino.com/thumbs/m8amIn1kQ5CGLLUXMQEOxQ_pb_x600.png',
+    description: 'Dökkur, ógagnsær, næstum svartrauður litur tekur á sig fjólubláan blæ meðan það er ungt en breytist í gulbrúna tóna með aldrinum. Vínið er sannkölluð sprenging af rauðum ávöxtum í nefi en býður uppá fínlegan blóma- og ákafan piparkeim. Ég get fátt fleira útskýrt - kauptu þér flösku hjá Sante, smakkaðu, gefðu vinum og njóttu.',
     seller: 'Sante',
-    price: '4.900 kr',
-    grapes: 'Tempranillo 80%, Garnacha 15%, Mazuelo 3%, Graciano 2%.',
-    country: 'Spánn',
+    price: '3.800 kr',
+    grapes: 'Syrah',
+    country: 'Frakkland - Rhone',
+    link_store: 'https://sante.is/products/2018-domaine-mucyn-cornas-cuvee-hypsos',
+    link_vivino: 'https://www.vivino.com/US-CA/en/mucyn-cornas-hypsos/w/10617154',
+    rating: '4.2',
     wineType: 'Rauðvín',
     wineColor: 'text-[#a82548]'
   }
   , {
-    name: '2011 Viña Bosconia Tinto Reserva',
-    img: 'https://images.vivino.com/thumbs/el1n78g-RgOjLEheat2MRA_pb_x600.png',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.',
-    seller: 'Sante',
-    price: '4.900 kr',
-    grapes: 'Tempranillo 80%, Garnacha 15%, Mazuelo 3%, Graciano 2%.',
-    country: 'Spánn',
+    name: 'The Bernard Series Pinotage',
+    img: 'https://images.vivino.com/thumbs/W_Aae_RVR8uMSSLNkq3plw_pb_x600.png',
+    description: 'Settu nefið að glasinu og það grípur þig afbragðsilmur af sólberjum, hindberjum og kirsuberjum. Hallaðu þér aftur og hugsaðu hvað lífið er gott. Fáðu þér sopa og finndu fullt, þurrt og hóflega sýrt vínið leika um munninn. Kyngdu svo og mundu að þetta er bara vín og það eru allir að horfa á þig. En þetta er frábært vín!',
+    seller: 'ÁTVR',
+    price: '3.999 kr',
+    grapes: 'Pinotage',
+    country: 'Suður Afríka',
+    link_store: 'https://www.vinbudin.is/heim/vorur/stoek-vara.aspx/?productid=28880',
+    link_vivino: 'https://www.vivino.com/US-CA/en/za-bellingham-the-bernard-series-bush-vine-limited-release-pinotage/w/7319906',
+    rating: '4.2',
     wineType: 'Rauðvín',
     wineColor: 'text-[#a82548]'
-  }]
+  }, {
+    name: 'Barahonda Summum',
+    img: 'https://images.vivino.com/thumbs/rAAgKLNZQ4WSgLR6IS5bKw_pb_x600.png',
+    description: 'Spænska Monstrell þrúgan hefur þykkt skinn sem gefur af sér dökk vín með öflug tannín og dáleiðandi dýpt. Hér höfum við vín sem sest að í munninum, húðar hann jafnvel. Hér finnum við fyrir sykurkenndum ávöxtum eins og kirskuberjum og plómum. Þetta er ákveðið vín með karakter og það mun svo sannarlega ekki versna með aldrinum.',
+    seller: 'ÁTVR',
+    price: '4.499 kr',
+    grapes: 'Monastrell',
+    country: 'Spánn- Murcia',
+    link_store: 'https://www.vinbudin.is/heim/vorur/stoek-vara.aspx/?productid=25872',
+    link_vivino: 'https://www.vivino.com/US-CA/en/barahonda-summum-monastrell/w/1679600',
+    rating: '4.2',
+    wineType: 'Rauðvín',
+    wineColor: 'text-[#a82548]'}]
 
   const props = defineProps({
   wines: Array
@@ -122,7 +146,7 @@ const items1 = [{
 <div class="flex justify-center items-center py-20">
   <div class="max-w-screen-md w-full px-4 font-teko text-[#3E3737]">
     <div class=" font-teko tracking-wider mb-7 text-[#FDA47E]">
-      <h3 class="text-4xl md:text-5xl">Frábær kaup undir 3.000 kr:</h3>
+      <h3 class="text-4xl md:text-5xl">Frábær rauðvín undir 3.000 kr:</h3>
       <p>Aðgengileg vín á góðu verði sem fást víða.</p>
     </div>
     <UCarousel v-slot="{ item }" :items="items1" :ui="{ item: 'basis-full' }" arrows class="max-h-screen bg-[#FFE7D4] rounded-lg p-3 sm:p-[5%] ">
@@ -131,6 +155,14 @@ const items1 = [{
         <div class="grid grid-cols-1 gap-3 lg:gap-5">
           <p class="text-xl" :class="`${item.wineColor}`">{{item.wineType}}</p>
           <h2 class="text-4xl md:text-5xl font-medium">{{ item.name }}</h2>
+          <div>
+            <div>
+              <span class="text-md sm:text-lg">{{ item.country }}</span>
+            </div>
+            <div>
+              <span class="text-md sm:text-lg text-[#7a7a7a]">LAND</span>
+            </div>
+          </div>
           <div>
             <div>
               <span class="text-md sm:text-lg">{{ item.description }}</span>
@@ -165,16 +197,26 @@ const items1 = [{
   </div>
 </div>
 
-<!--<div class="flex justify-center items-center py-20 bg-[#2A2424]">
+<div class="flex justify-center items-center py-20">
   <div class="max-w-screen-md w-full px-4 font-teko text-[#3E3737]">
-    <div class="text-4xl md:text-5xl font-teko tracking-wider mb-7  text-[#FDA47E]">
-      <h3>Nokkur skemmtileg:</h3>
+    <div class=" font-teko tracking-wider mb-7 text-[#FDA47E]">
+      <h3 class="text-4xl md:text-5xl">Frábær rauðvín upp að 5.000 kr:</h3>
+      <p>Algjör gæðavín - gætu klárast fljótlega.</p>
     </div>
     <UCarousel v-slot="{ item }" :items="items2" :ui="{ item: 'basis-full' }" arrows class="max-h-screen bg-[#FFE7D4] rounded-lg p-3 sm:p-[5%] ">
-      <img title="Highlighted_product" :src="item.img" :alt="item.name" draggable="false" loading="lazy"  class="w-auto max-h-[70vh] md:max-h-full">
+      <img title="Highlighted_product" :src="item.img" :alt="item.name" draggable="false" loading="lazy" class="w-auto max-h-[70vh] md:max-h-full">
       <div class="flex flex-col p-5">
         <div class="grid grid-cols-1 gap-3 lg:gap-5">
-          <h2 class="text-5xl font-medium">{{ item.name }}</h2>
+          <p class="text-xl" :class="`${item.wineColor}`">{{item.wineType}}</p>
+          <h2 class="text-4xl md:text-5xl font-medium">{{ item.name }}</h2>
+          <div>
+            <div>
+              <span class="text-md sm:text-lg">{{ item.country }}</span>
+            </div>
+            <div>
+              <span class="text-md sm:text-lg text-[#7a7a7a]">LAND</span>
+            </div>
+          </div>
           <div>
             <div>
               <span class="text-md sm:text-lg">{{ item.description }}</span>
@@ -207,5 +249,5 @@ const items1 = [{
       </div>
     </UCarousel>
   </div>
-</div> -->
+</div>
 </template>
